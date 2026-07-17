feb68865f25c7a346dd992a0e96bd87a6e2a1d05
nikhildandodkar <dnikhil93@gmail.com>
2026-07-17 06:39:04 +0530

    [Runtime] Fixed code_start_here() with correct return type int64_t

--- src/runner.c
+++ src/runner.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdint.h>
-extern int code_starts_here() asm("code_starts_here");
+extern int64_t code_starts_here() asm("code_starts_here");
 
 int main(int argc, char** argv) {
   int64_t result = code_starts_here();
+  
   if(result&1){
 	  printf("%ld\n", result/2);
-  }else if (result==0xFFFFFFFFFFFFFFFE){
-	  printf("true\n");
-  }else if (result==0x7FFFFFFFFFFFFFFE){
+  }else if (result==0xFFFFFFFFFFFFFFFEULL){
+  	  printf("true\n");
+	  
+  }else if (result==0x7FFFFFFFFFFFFFFEULL){
 	  printf("false\n");
   }else{
 	  printf("error: value not representable\n");
